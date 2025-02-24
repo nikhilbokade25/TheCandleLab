39d14e47215ae11004b42e44e66d7395645ece83
Nikhil Bokade <nikhilbokade25@gmail.com>
2025-02-24 10:16:24 -0500

    Integrated product data into shop component

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -3,48 +3,50 @@ const PRODUCT_DETAILS = [
         id: 1,
         name: "MAHOGANY COCONUT",
         price: "$15.99",
-        image: "../Assets/candle_1.jpg"
+        image: require("../Assets/candle_1.jpg")
     },
     {
         id: 2,
         name: "BALSAM CEDAR",
         price: "$13.99",
-        image: "../Assets/candle_2.jpg"
+        image: require("../Assets/candle_2.jpg")
     },
     {
         id: 3,
         name: "ROSE SANDALWOOD",
         price: "$12.99",
-        image: "../Assets/candle_3.jpg"
+        image: require("../Assets/candle_3.jpg")
     },
     {
         id: 4,
         name: "BEACH DAY",
         price: "$9.99",
-        image: "../Assets/candle_4.jpg"
+        image: require("../Assets/candle_4.jpg")
     },
     {
         id: 5,
         name: "FRENCH LAVENDER",
         price: "$19.99",
-        image: "../Assets/candle_5.jpg"
+        image: require("../Assets/candle_5.jpg")
     },
     {
         id: 6,
         name: "LAVENDER VANILLA",
         price: "$21",
-        image: "../Assets/candle_6.jpg"
+        image: require("../Assets/candle_6.jpg")
     },
     {
         id: 7,
         name: "BLACKBERRY VANILLA",
         price: "$9.99",
-        image: "../Assets/candle_7.jpg"
+        image: require("../Assets/candle_7.jpg")
     },
     {
         id: 8,
         name: "EUCALYPTUS & MINT",
         price: "$10.99",
-        image: "../Assets/candle_8.jpg"
+        image: require("../Assets/candle_8.jpg")
     }
 ];
+
+export default PRODUCT_DETAILS;
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,36 +1,44 @@
 import './Shop.css';
+import PRODUCT_DETAILS from './Data';
+import { FaHeart } from 'react-icons/fa';
 
 export default function Shop() {
   return (
     <div className="shop_container">
       <div className="shop_products">
-        <div className="product">
-
-        </div>
-        <div className="product">
-
-        </div>
-        <div className="product">
-
-        </div>
-        <div className="product">
-
-        </div>
+        {PRODUCT_DETAILS
+          .filter((product) => product.id >= 1 && product.id <= 4)
+          .map((product) => (
+            <div key={product.id} className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="product_details">
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+              </div>
+              <div className="product_buttons">
+                <button className="btn">ADD TO CART</button>
+                <button className="btn"><FaHeart /></button>
+              </div>
+            </div>
+          ))}
       </div>
 
       <div className="shop_products">
-        <div className="product">
-
-        </div>
-        <div className="product">
-
-        </div>
-        <div className="product">
-
-        </div>
-        <div className="product">
-            
-        </div>
+        {PRODUCT_DETAILS
+          .filter((product) => product.id >= 5 && product.id <= 8)
+          .map((product) => (
+            <div key={product.id} className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="product_details">
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+              </div>
+              <div className="product_buttons">
+                <button className="btn">ADD TO CART</button>
+                <button className="btn"><FaHeart /></button>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
